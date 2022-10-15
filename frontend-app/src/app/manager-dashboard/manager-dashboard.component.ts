976ac2766d19dd0b5e6a5efdf0e9c00ec1721f38
U A Ganesh <ganesh.ua9@gmail.com>
2022-10-15 11:19:57 +0530

    frontend app

---
+++ frontend-app/src/app/manager-dashboard/manager-dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-dashboard',
+  templateUrl: './manager-dashboard.component.html',
+  styleUrls: ['./manager-dashboard.component.css']
+})
+export class ManagerDashboardComponent implements OnInit {
+
+  complaint:string="image/complaint.png"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
